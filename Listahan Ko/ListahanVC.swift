13cbdd8c761f3ea759f624090819bb5e73654108
jervygu <jervygu@gmail.com>
2020-06-18 13:29:27 +0800

    Listahan ko App Complete with datasource and delegate methods

--- Listahan Ko/ListahanVC.swift
+++ Listahan Ko/ListahanVC.swift
@@ -41,9 +41,25 @@ class ListahanVC: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "listahanCell", for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
         
+        
+        
         return cell
     }
     
+    
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     
     
     
