65857c096c726b0cc55edbe08f2fd5a43b05068e
jervygu <jervygu@gmail.com>
2020-06-24 20:17:53 +0800

    Persisting data using Core Data completed

---
+++ Listahan Ko/Controller/CategoryVC.swift
@@ -0,0 +1,113 @@
+//
+//  CategoryVC.swift
+//  Listahan Ko
+//
+//  Created by Jaypee Umandap on 6/24/20.
+//  Copyright © 2020 Jervy Umandap. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryVC: UITableViewController {
+    
+    var categoryArray = [Category]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        
+        
+        loadCategories()
+        
+    }
+    
+    //MARK: - TableView DataSource Methods
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoryArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath)
+        
+        let categories = categoryArray[indexPath.row]
+        
+        cell.textLabel?.text = categories.name
+        
+        return cell
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new category", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add category", style: .default) { (action) in
+            //            will happen once the user clicks Add item button on UIAlert
+            print("Success adding category \(String(describing: textField.text!))")
+            
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text!
+            
+            self.categoryArray.append(newCategory)
+            
+            self.saveCategories()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Type new category here."
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    //MARK: - Tableview Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! ListahanVC
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = categoryArray[indexPath.row]
+        }
+    }
+    
+    //MARK: - Data Manipulation Methods
+    
+    func saveCategories() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    //  with - external ... request - internal param ... = Item.fetchRequest() - default value
+    
+    func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        do {
+            categoryArray =  try context.fetch(request)
+        } catch {
+            print("Error fetching categories from context \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+}
